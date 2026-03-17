16d22cfbe9d7ed27a0adf7dd12b92cceb2248a2b
Kerem Gurkan <kegu3575@colorado.edu>
2026-03-17 23:56:47 +0100

    Adds chessboard to UI

--- src/main/java/org/chess/ChessApp.java
+++ src/main/java/org/chess/ChessApp.java
@@ -10,20 +10,17 @@ import java.util.Objects;
 
 
 public class ChessApp extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/chess/scene.fxml")));
-
-        Scene scene = new Scene(root);
+        ChessBoard board = new ChessBoard();
+        Scene scene = new Scene(board);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/org/chess/styles.css")).toExternalForm());
 
-        stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
         stage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/org/chess/ChessBoard.java
@@ -0,0 +1,54 @@
+package org.chess;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+public class ChessBoard extends BorderPane {
+    private final GridPane boardGrid = new GridPane();
+    private final StackPane[][] squares = new StackPane[8][8];
+    private final Label[][] pieceLabels = new Label[8][8];
+
+    public ChessBoard() {
+        buildBoardGrid();
+
+        setCenter(boardGrid);
+
+        refreshBoard();
+    }
+
+    private void buildBoardGrid() {
+        // Note: Avoiding dealing with square styles here to prevent repeat in refreshBoard which will need to deal with UI changes for available moves
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Label pieceLabel = new Label("T");
+                StackPane square = new StackPane(pieceLabel);
+
+                square.setMinSize(72, 72);
+                square.setPrefSize(72, 72);
+
+                square.setOnMouseClicked(event -> System.out.println("mouse clicked--useful for later"));
+
+                squares[row][col] = square;
+                pieceLabels[row][col] = pieceLabel;
+                boardGrid.add(square, col, row);
+            }
+        }
+    }
+
+    private void refreshBoard() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                StackPane square = squares[row][col];
+                square.getStyleClass().clear();
+
+                if ((row + col) % 2 == 0) {
+                    square.getStyleClass().add("board-square-beige");
+                } else {
+                    square.getStyleClass().add("board-square-olive");
+                }
+            }
+        }
+    }
+}
